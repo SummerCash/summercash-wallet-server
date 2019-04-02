6032860b0b74e363d1de39c6ecdad8c0296a250b
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-01 20:36:32 -0400

    accounts: Created accounts_db.go.

---
+++ accounts/accounts_db.go
@@ -0,0 +1,7 @@
+// Package accounts defines account-related helper methods and types.
+// The accounts database, for example, is defined in this package.
+package accounts
+
+// DB is a data type representing a link to a working accounts boltdb instance.
+type DB struct {
+}
