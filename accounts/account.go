dd352359ea17fb2025051ca1c3deee10bcdff8c0
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-01 21:19:00 -0400

    accounts: Account serialization.

--- accounts/account.go
+++ accounts/account.go
@@ -2,13 +2,42 @@
 // The accounts database, for example, is defined in this package.
 package accounts
 
-import "github.com/polaris-project/go-polaris/common"
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/SummerCash/go-summercash/common"
+)
 
 // Account represents a username-password keypair linking to a private key in the accounts database.
 type Account struct {
 	Name string `json:"name"` // Name
 
-	Password string `json:"password"` // Password
+	PasswordHash []byte `json:"password_hash"` // Password hash
 
-	Address *common.Address `json:"address"` // Address
+	Address common.Address `json:"address"` // Address
 }
+
+/* BEGIN EXPORTED METHODS */
+
+// AccountFromBytes deserializes an account from a given byte array.
+func AccountFromBytes(b []byte) (*Account, error) {
+	account := Account{} // Init buffer
+
+	err := json.NewDecoder(bytes.NewReader(b)).Decode(&account) // Decode into buffer
+
+	if err != nil { // Check for errors
+		return nil, err // Return found error
+	}
+
+	return &account, nil // No error occurred, return read value
+}
+
+// Bytes serializes a given account to a byte array.
+func (account *Account) Bytes() []byte {
+	marshaledVal, _ := json.MarshalIndent(*account, "", "  ") // Marshal
+
+	return marshaledVal // Return marshaled val
+}
+
+/* END EXPORTED METHODS */
---
+++ accounts/account_test.go
@@ -0,0 +1,42 @@
+// Package accounts defines account-related helper methods and types.
+// The accounts database, for example, is defined in this package.
+package accounts
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/SummerCash/go-summercash/common"
+
+	"github.com/SummerCash/summercash-wallet-server/crypto"
+)
+
+/* BEGIN EXPORTED METHODS TESTS */
+
+// TestBytesAccount tests the functionality of the Bytes() helper method.
+func TestBytesAccount(t *testing.T) {
+	address, err := common.StringToAddress("0x040028d536d5351e83fbbec320c194629ace") // Get addr value
+
+	if err != nil { // Check for errors
+		t.Error(err) // Log found error
+		t.FailNow()  // Panic
+	}
+
+	account := &Account{
+		Name:         "Dowland Aiello",                               // Set name
+		PasswordHash: crypto.Sha3([]byte("despacito_despacito_lol")), // Set password
+		Address:      address,                                        // Set address
+	}
+
+	deserializedAccount, err := AccountFromBytes(account.Bytes()) // Deserialize account bytes
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+
+	if !bytes.Equal(deserializedAccount.Address.Bytes(), address.Bytes()) { // Check address not matching
+		t.Fatal("address deserialized incorrectly") // Panic
+	}
+}
+
+/* END EXPORTED METHODS TESTS */
