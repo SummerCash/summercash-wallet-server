3588eb2642094bde6193f19c01724604026433e8
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-04 17:38:48 -0400

    api/standardapi: Wrote NewTx() functionality.

--- api/standardapi/transactions_api.go
+++ api/standardapi/transactions_api.go
@@ -2,9 +2,15 @@
 package standardapi
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/SummerCash/summercash-wallet-server/common"
+	"github.com/SummerCash/summercash-wallet-server/transactions"
+
 	"github.com/valyala/fasthttp"
+
+	summercashCommon "github.com/SummerCash/go-summercash/common"
 )
 
 /* BEGIN EXPORTED METHODS */
@@ -20,7 +26,41 @@ func (api *JSONHTTPAPI) SetupTransactionsRoutes() error {
 
 // NewTransaction handles a NewTransaction request.
 func (api *JSONHTTPAPI) NewTransaction(ctx *fasthttp.RequestCtx) {
+	recipient, err := summercashCommon.StringToAddress(string(common.GetCtxValue(ctx, "recipient"))) // Parse recipient
 
+	if err != nil { // Check for errors
+		logger.Errorf("errored while handling NewTransaction request with username %s: %s", string(common.GetCtxValue(ctx, "username")), err.Error()) // Log error
+
+		panic(err) // Panic
+	}
+
+	var amountJSONVal *json.Number // Init amount JSON buffer
+
+	err = json.Unmarshal(common.GetCtxValue(ctx, "amount"), amountJSONVal) // Unmarshal
+
+	if err != nil { // Check for errors
+		logger.Errorf("errored while handling NewTransaction request with username %s: %s", string(common.GetCtxValue(ctx, "username")), err.Error()) // Log error
+
+		panic(err) // Panic
+	}
+
+	amount, err := amountJSONVal.Float64() // Get f64 val
+
+	if err != nil { // Check for errors
+		logger.Errorf("errored while handling NewTransaction request with username %s: %s", string(common.GetCtxValue(ctx, "username")), err.Error()) // Log error
+
+		panic(err) // Panic
+	}
+
+	transaction, err := transactions.NewTransaction(api.AccountsDatabase, string(common.GetCtxValue(ctx, "username")), string(common.GetCtxValue(ctx, "password")), &recipient, amount, common.GetCtxValue(ctx, "payload")) // Initialize transaction
+
+	if err != nil { // Check for errors
+		logger.Errorf("errored while handling NewTransaction request with username %s: %s", string(common.GetCtxValue(ctx, "username")), err.Error()) // Log error
+
+		panic(err) // Panic
+	}
+
+	fmt.Fprintf(ctx, transaction.String()) // Write tx string value
 }
 
 /* END EXPORTED METHODS */
