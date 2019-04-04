afe9995b4fe52f397d0852702469cc50dbcfc169
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-03 21:18:29 -0400

    api/standardapi: Final commit OTD.

--- api/standardapi/transactions_api.go
+++ api/standardapi/transactions_api.go
@@ -1,2 +1,26 @@
 // Package standardapi defines the summercash-wallet-server API.
 package standardapi
+
+import (
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
+
+/* BEGIN EXPORTED METHODS */
+
+// SetupTransactionsRoutes sets up all the transactions api-related routes.
+func (api *JSONHTTPAPI) SetupTransactionsRoutes() error {
+	transactionsAPIRoot := "/api/transactions" // Get transactions API root path
+
+	api.Router.POST(fmt.Sprintf("%s/NewTransaction", transactionsAPIRoot), api.NewTransaction) // Set NewTransaction post
+
+	return nil // No error occurred, return nil
+}
+
+// NewTransaction handles a NewTransaction request.
+func (api *JSONHTTPAPI) NewTransaction(ctx *fasthttp.RequestCtx) {
+
+}
+
+/* END EXPORTED METHODS */
