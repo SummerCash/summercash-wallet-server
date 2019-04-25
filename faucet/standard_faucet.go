69ba40ee8a270587a6dfa6877afaeee3e4040a73
Dowland Aiello <mitsukomegumii@gmail.com>
2019-04-25 09:49:43 +0200

    faucet: Removed input modifying code.

--- faucet/standard_faucet.go
+++ faucet/standard_faucet.go
@@ -44,9 +44,7 @@ func (faucet *StandardFaucet) AccountCanClaim(account *accounts.Account) bool {
 		return false // Cannot claim; does not exist
 	}
 
-	account = updatedAccount // Set to updated account reference
-
-	if account.LastFaucetClaimTime.Sub(time.Now()).Hours() < 24 { // Check less than 24 hours since last claim
+	if updatedAccount.LastFaucetClaimTime.Sub(time.Now()).Hours() < 24 { // Check less than 24 hours since last claim
 		return false // Cannot claim
 	}
 
@@ -61,21 +59,25 @@ func (faucet *StandardFaucet) AccountLastClaim(account *accounts.Account) time.T
 		return time.Date(2017, time.January, 12, 0, 0, 0, 0, time.UTC) // Return despacito music video publish date
 	}
 
-	account = updatedAccount // Set to updated account reference
-
-	return account.LastFaucetClaimTime // Return last claim time
+	return updatedAccount.LastFaucetClaimTime // Return last claim time
 }
 
 // AmountCanClaim gets the max amount an account can claim.
 // If the account has already claimed in the last 24 hours, zero is returned.
 func (faucet *StandardFaucet) AmountCanClaim(account *accounts.Account) *big.Float {
+	updatedAccount, err := faucet.AccountsDatabase.QueryAccountByUsername(account.Name) // Set to updated account
+
+	if err != nil { // Check for errors
+		return big.NewFloat(0)
+	}
+
 	for _, user := range faucet.Ruleset.BannedUsers() { // Iterate through banned users
-		if bytes.Equal(user.Address.Bytes(), account.Address.Bytes()) { // Check is banned user
+		if bytes.Equal(user.Address.Bytes(), updatedAccount.Address.Bytes()) { // Check is banned user
 			return big.NewFloat(0) // Return zero
 		}
 	}
 
-	if faucet.AccountLastClaim(account).Sub(time.Now()).Hours() < 24 { // Check less than 24 hours since last claim
+	if faucet.AccountLastClaim(updatedAccount).Sub(time.Now()).Hours() < 24 { // Check less than 24 hours since last claim
 		return big.NewFloat(0) // Cannot claim
 	}
 
@@ -90,13 +92,11 @@ func (faucet *StandardFaucet) Claim(account *accounts.Account, amount *big.Float
 		return err // Return found error
 	}
 
-	account = updatedAccount // Set account updated reference
-
-	if amountCanClaim := faucet.AmountCanClaim(account); amountCanClaim.Cmp(amount) == -1 { // Check amount to claim less than can claim
+	if amountCanClaim := faucet.AmountCanClaim(updatedAccount); amountCanClaim.Cmp(amount) == -1 { // Check amount to claim less than can claim
 		return ErrInvalidClaimSize // Return error
 	}
 
-	err = faucet.WorkingDB().MakeFaucetClaim(account, amount) // Make faucet claim
+	err = faucet.WorkingDB().MakeFaucetClaim(updatedAccount, amount) // Make faucet claim
 
 	if err != nil { // Check for errors
 		return err // Return found error
